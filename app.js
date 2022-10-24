7658ee69c0d5357d7d4ae1f7f327dc12578c3f53
Henry Dufour <henrydufourdiscord@gmail.com>
2022-10-24 15:44:08 -0400

    reworked code

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ const app = Express()
 
 const PORT = 4000
 
-const oneRoute = require("./controllers/one")
-const twoRoute = require("./controllers/two")
-const threeRoute = require("./controllers/three")
+// const oneRoute = require("./controllers/one")
+// const twoRoute = require("./controllers/two")
+// const threeRoute = require("./controllers/three")
 
 
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send(`<h1>hello from the home page</h1>`)
+})
+
+app.get("/about", (req, res) => {
+    res.send(`<h1>hello from the ${req.originalUrl} page</h1>`)
+})
+
+app.get("/contact", (req, res) => {
+    res.send(`<h1>hello from the ${req.originalUrl} page</h1>`)
+})
+
+app.get("/:page", (req, res) => {
+    res.send(`your at ${req.params.page}`)
+})
+
+app.get("*", (req, res) => {
+    res.send("404 route not found")
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
-
-function logPage(req, res, next) {
-    let date = new Date()
-    console.log(date.toLocaleDateString());
-    next()
-}
-
-app.use(oneRoute)
-app.use(twoRoute)
-app.use(threeRoute)
-
-// app.get("/:home", (req, res) => {
-//     let home = req.params.home
-//     res.send(`hello ${home}`)
-// })
